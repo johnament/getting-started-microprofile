c6608b68c0686cd2935d3802784f923305beb3a2
Ondrej Mihalyi <ondrej.mihalyi@payara.fish>
2018-04-07 11:20:30 +0200

    Fixed injection of principal and claims on Payara Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@payara.fish>

--- src/main/java/ws/ament/microprofile/gettingstarted/BookServiceConfig.java
+++ src/main/java/ws/ament/microprofile/gettingstarted/BookServiceConfig.java
@@ -1,5 +1,6 @@
 package ws.ament.microprofile.gettingstarted;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import org.eclipse.microprofile.auth.LoginConfig;
@@ -9,8 +10,9 @@ import org.eclipse.microprofile.auth.LoginConfig;
  * @author Ondrej Mihalyi
  */
 @LoginConfig(authMethod = "MP-JWT", realmName = "admin-realm")
+@ApplicationScoped
 @ApplicationPath("/")
-public class ServiceConfig extends Application {
+public class BookServiceConfig extends Application {
 
 }
 
